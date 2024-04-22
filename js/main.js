67fcef607f3c22e08b174eeba2015af43839f8ea
dccontreras50 <73891538+dccontreras50@users.noreply.github.com>
2024-04-21 22:44:18 -0500

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function circuloFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("circulo");
+}
+
+
+function romboFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("rombo");
+}
+
+function rectanguloFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("rectangulo");
+}
+
+
+function topFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("top");
+}
+
+function rightFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("right");
+
+}
+
+function bottomFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("bottom");
+}
+
+function leftFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("left");
+}
+
+function diagderFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("diagder")
+}
+
+function diagizqFunction(){
+    var selector = document.getElementById("figura");
+    selector.className = '';
+    selector.classList.toggle("diagizq");
+}
